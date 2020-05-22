582c18d41a4eb5d5c56cf159d0f91384e57d9dec
ElianMelo <elianmelomorais@gmail.com>
2020-05-22 20:09:30 -0300

    Leitura do financiamento

--- src/financiamento/Programa.java
+++ src/financiamento/Programa.java
@@ -10,7 +10,8 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 		
 		String nome;
-		double salario;
+		double salario, financiado, parcela, entrada;
+		int prestacoes, porcentagem;
 		
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
 		System.out.print("Nome do cliente: ");
@@ -20,6 +21,22 @@ public class Programa {
 		while(salario < 0.0) {
 			System.out.print("Salario: ");
 			salario = sc.nextDouble();
+		}
+		
+		System.out.print("Numero de prestacoes: ");
+		prestacoes = sc.nextInt();
+		System.out.print("Porcentagem de entrada: ");
+		porcentagem = sc.nextInt();
+		System.out.print("Valor total financiado: ");
+		financiado = sc.nextDouble();
+		entrada = financiado * porcentagem / 100;
+		parcela = (financiado - entrada) / prestacoes;
+		
+		while (parcela > salario * 0.3) {
+			System.out.print("Valor total financiado: ");
+			financiado = sc.nextDouble();
+			entrada = financiado * porcentagem / 100;
+			parcela = (financiado - entrada) / prestacoes;
 		}
 		
 		sc.close();
